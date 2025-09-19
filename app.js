6947f530186e87b67ca3e173b8e8bee800c5d4dc
esteel7 <rodrigodr7@gmail.com>
2025-09-19 13:57:08 -0300

    Refactor app.js to enhance functionality and add user input in index.html

--- app.js
+++ app.js
@@ -3,3 +3,31 @@ function sayHello(name) {
 }
 
 sayHello("Rodrigo");
+
+let ten = 10;
+console.log(ten*ten);
+
+console.log(Math.max(2, 4));
+
+console.log(Number.isNaN("hola"));
+
+if (1+1 == 2) {
+    console.log("Es verdad");
+}
+
+let numero = 15;
+
+if (numero < 10){
+    console.log("Pequeño");
+} else if (numero < 100){
+    console.log("Mediano");
+}else{
+    console.log("Grande");
+}
+
+numero =0;
+
+while (numero<=12){
+    console.log(numero);
+    numero+=2;
+}
