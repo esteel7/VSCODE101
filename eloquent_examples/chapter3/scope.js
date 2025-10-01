522004a27abd94c65ce4da12fe8fd540bbe3672f
esteel7 <rodrigodr7@gmail.com>
2025-10-01 15:31:50 -0300

    Se agrega otro ejemplo a scope.js

--- eloquent_examples/chapter3/scope.js
+++ eloquent_examples/chapter3/scope.js
@@ -8,10 +8,11 @@ if (true){
 //console.log(y); // No imprimirá que sólo es "visible" dentro del bloque if
 console.log(z); // Imprime sin problemas
 
-/*
-En el siguiente ejemplo, cuando el código dentro de la función halve hace referencia 
-a n, está viendo su propio n, no el n global.
-*/
+//======================= ENLACES Y ÁMBITOS =======================//
+
+/* En el siguiente ejemplo, cuando el código dentro de la función halve 
+hace referencia a n, está viendo su propio n, no el n global.*/
+
 const halve = function(n){
     return n / 2;
 };
@@ -21,3 +22,22 @@ console.log(halve(100));
 // → 50
 console.log(n);
 // → 10
+
+//======================== ÁMBITO ANIDADO ========================//
+const hummus = function(factor) {
+
+    const ingredient = function(amount, unit, name) {
+        let ingredientAmount = amount * factor;
+        if (ingredientAmount > 1){
+            unit += "s";
+        };
+        console.log(`${ingredientAmount} ${unit} de ${name}`);
+    };
+    ingredient(1, "lata", "garbanzos");
+    ingredient(0.25, "taza", "tahini");
+    ingredient(0.25, "taza", "jugo de limón");
+    ingredient(1, "diente", "ajo");
+    ingredient(2, "cucharada", "aceite de oliva");
+    ingredient(0.5, "cucharada", "comino");
+};
+hummus(3);
