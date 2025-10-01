b7f096b3675e9de24ccfe3ea6c6ab4b299d50e50
esteel7 <rodrigodr7@gmail.com>
2025-10-01 13:36:04 -0300

    Se crean carpetas para organizar los ejemplos de EloquentJavaScript

--- eloquent_examples/chapter 3/funciones.js
+++ eloquent_examples/chapter 3/funciones.js
@@ -1,10 +1,17 @@
 const square = function(x){
     return x * x;
-}
+};
+
 let x = 12
 console.log(`El cuadrado de ${x} es ${square(12)}`);
 
 const makeNoise = function(){
     console.log("¡Pling!")
-}
+};
 
+const roundTo = function(n, step){
+    let resto = n % step;
+    return n - resto + (resto < step / 2 ? 0 : step);
+};
+
+console.log(roundTo(23, 10))
---
+++ eloquent_examples/chapter 3/scope.js
@@ -0,0 +1,8 @@
+let x = 10; //global
+
+if (true){
+    let y = 20; // local al bloque
+    var z = 30; // también global
+}
+
+console.log(z);
