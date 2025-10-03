d2e753c5ec44bf4edb795d41ff7cdf349bc4efda
esteel7 <rodrigodr7@gmail.com>
2025-10-03 11:44:52 -0300

    Se agregan ejemplos a funciones.js

--- eloquent_examples/chapter3/funciones.js
+++ eloquent_examples/chapter3/funciones.js
@@ -15,3 +15,21 @@ const roundTo = function(n, step){
 };
 
 console.log(roundTo(23, 10))
+
+//=============== Notación de Declaración ====================//
+// NO se requiere el punto y coma aquí. Se puede usar antes de su definición
+
+console.log("El futuro dice:", future());
+
+function future(){
+    return "Nunca tendrás autos voladores";
+}
+
+//============== Funciones de Flecha ======================//
+const paralelo = (a, b) => {return a*b/(a+b)};
+
+console.log(`El paralelo entre 10 y 10 es:  ${paralelo(10,10)}`);
+
+const exponente2 = x => x * x; //Si la función sólo tiene un argumento se puede hacer esto.
+
+console.log(`El ${2} al cuadrado es igual a: ${exponente2(2)}`);
